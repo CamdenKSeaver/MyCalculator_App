395ba4e59e60d984818786c4de721e4297b50197
CamdenKSeaver <Camdenkseaver@gmail.com>
2022-08-22 08:19:26 -0500

    finished methods

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mycalculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void findSum(View view) {
+        EditText number1ET = findViewById(R.id.number);
+        EditText number2ET = findViewById(R.id.number2);
+        TextView numberSumTV = findViewById(R.id.result);
+
+        double num1 = Integer.parseInt((number1ET.getText().toString()));
+        double num2 = Integer.parseInt((number2ET.getText().toString()));
+        double sum = num1 + num2;
+
+        numberSumTV.setText("" + sum);
+    }
+    public void findSub(View view) {
+        EditText number1ET = findViewById(R.id.number);
+        EditText number2ET = findViewById(R.id.number2);
+        TextView numberSumTV = findViewById(R.id.result);
+
+        double num1 = Integer.parseInt((number1ET.getText().toString()));
+        double num2 = Integer.parseInt((number2ET.getText().toString()));
+        double sum = num1 - num2;
+
+        numberSumTV.setText("" + sum);
+    }
+    public void findProduct(View view) {
+        EditText number1ET = findViewById(R.id.number);
+        EditText number2ET = findViewById(R.id.number2);
+        TextView numberSumTV = findViewById(R.id.result);
+
+        double num1 = Integer.parseInt((number1ET.getText().toString()));
+        double num2 = Integer.parseInt((number2ET.getText().toString()));
+        double sum = num1 * num2;
+
+        numberSumTV.setText("" + sum);
+    }
+
+    public void findDiv(View view) {
+        EditText number1ET = findViewById(R.id.number);
+        EditText number2ET = findViewById(R.id.number2);
+        TextView numberSumTV = findViewById(R.id.result);
+
+        double num1 = Integer.parseInt((number1ET.getText().toString()));
+        double num2 = Integer.parseInt((number2ET.getText().toString()));
+        double sum = num1 / num2;
+
+        numberSumTV.setText("" + sum);
     }
 }
